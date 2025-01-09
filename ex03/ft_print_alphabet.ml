47389f8399d6ffe902445b99fb64a15a21dce814
Victor Cornille <victor.bolheme@gmail.com>
2025-01-09 14:10:13 +0100

    add: Day0 ex03

---
+++ ex03/ft_print_alphabet.ml
@@ -0,0 +1,14 @@
+let ft_print_alphabet () =
+        let rec loop x =
+                if x > 122 then
+                        ()
+                else begin
+                        print_char (char_of_int x);
+                        loop (x + 1)
+                end
+        in
+        loop 97;
+        print_char '\n';;
+
+(* TEST *)
+ft_print_alphabet ();;
