8d508ccd4951b83dd4fc1ea4b7a2354b700c8fa8
Victor Cornille <victor.bolheme@gmail.com>
2025-01-09 15:32:47 +0100

    add: day0 ex07

---
+++ day0/ex07/ft_is_palindrome.ml
@@ -0,0 +1,20 @@
+let ft_is_palindrome str =
+        let str_len = String.length str - 1 in
+        if str_len = -1 then
+                true
+        else
+                let rec loop i =
+                        if i > str_len / 2 then
+                                true
+                        else if String.get str i <> String.get str (str_len - i) then
+                                false
+                        else
+                                loop (i + 1)
+                in
+                loop 0;;
+
+(* TESTS *)
+assert (ft_is_palindrome "abba" = true);;
+assert (ft_is_palindrome "abtba" = true);;
+assert (ft_is_palindrome "abtbaa" = false);;
+print_endline "ALL TESTS PASS"
