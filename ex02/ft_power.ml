12a81fcf853c4ae81164508ff8ad207e60e23856
Victor Cornille <victor.bolheme@gmail.com>
2025-01-09 13:54:24 +0100

    add: Day0 ex02

---
+++ ex02/ft_power.ml
@@ -0,0 +1,22 @@
+let rec rec_power x y z =
+        if y = 0 then
+                z
+        else
+                rec_power x (y - 1) (z * x);;
+
+
+let ft_power x y = 
+        if x = 0 then
+                0
+        else if y = 0 then
+                1
+        else if y = 1 then
+                x
+        else 
+                rec_power x y 1;;
+
+(* TEST *)
+assert (ft_power 2 4 = 16);;
+assert (ft_power 3 0 = 1);;
+assert (ft_power 0 5 = 0);;
+print_endline "TEST SUCCEDED!"
