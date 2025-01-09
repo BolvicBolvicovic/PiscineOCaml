2b6e5946edb0568b27a2735f168fa9adf44c48f1
Victor Cornille <victor.bolheme@gmail.com>
2025-01-09 15:11:51 +0100

    add: day0 ex06

---
+++ day0/ex06/ft_string_all.ml
@@ -0,0 +1,18 @@
+let predicate_char_is_a char = char = 'a';;
+
+let ft_string_all predicate str =
+        let str_len = String.length str in
+        let rec loop i =
+                if i < str_len then
+                        if predicate (String.get str i) = false then
+                                false
+                        else loop (i + 1)
+                else
+                        true
+        in
+        loop 0;;
+
+(* TESTS *)
+assert (ft_string_all predicate_char_is_a "aaaaaaaaaaaaa" = true);;
+assert (ft_string_all predicate_char_is_a "aaaaaadaaaa" = false);;
+print_endline "ALL TESTS PASS"
